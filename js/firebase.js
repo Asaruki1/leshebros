258a888b2db9c207ad6f9a70bd35d40f3d83100d
Asaruki1 <148998099+Asaruki1@users.noreply.github.com>
2025-09-15 14:47:13 +0700

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBFK98q0ApvdD7H45VV7hyIrjx0l8N5pnI",
+  authDomain: "leshebros.firebaseapp.com",
+  databaseURL: "https://leshebros-default-rtdb.firebaseio.com",
+  projectId: "leshebros",
+  storageBucket: "leshebros.firebasestorage.app",
+  messagingSenderId: "92936380725",
+  appId: "1:92936380725:web:9c6a70acc2cb3c3db9ec7c",
+  measurementId: "G-HMXVSDQLP7"
+};
+firebase.initializeApp(firebaseConfig);
+const db=firebase.database();
